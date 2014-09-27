f273bfb6bac1d1083ddbb21adc51e68e896dc6b8
amayausky <aaron.mayausky@gmail.com>
2014-09-27 15:10:43 -0400

    adding three levels of solution, works, better, best using overview and research

--- problem_1.py
+++ problem_1.py
@@ -1,1 +1,15 @@
-print sum([i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0])
+target = 1000
+
+# works
+num_sum = 0
+for i in range(1, target):
+    if i % 3 == 0 or i % 5 == 0:
+        num_sum += i
+print num_sum
+
+# better
+print sum([i for i in range(1, target) if i % 3 == 0 or i % 5 == 0])
+
+# best
+quick_sum = lambda n, p: n*(p*(p + 1))/2
+print quick_sum(3, target/3) + quick_sum(5, target/5) - quick_sum(15, target/15)
